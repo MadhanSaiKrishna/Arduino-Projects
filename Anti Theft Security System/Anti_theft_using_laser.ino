ea50ed01d5b5d3121043e6977f1dd83df9d44ffa
Madhan Sai Krishna <147420539+MadhanSaiKrishna@users.noreply.github.com>
2023-12-29 17:45:18 +0530

    Update Anti_theft_using_laser.ino

--- Anti Theft Security System/Anti_theft_using_laser.ino
+++ Anti Theft Security System/Anti_theft_using_laser.ino
@@ -1,17 +1,22 @@
 int ldrPin = A0;
 int buzzerPin = 7;
 
+#define THRESHOLD 550
+
 void setup() {
   pinMode(ldrPin, INPUT);
   pinMode(buzzerPin, OUTPUT);
+  // Serial.begin(9600);
 }
 
 void loop() {
   
   int ldrValue = analogRead(ldrPin);
   delay(10);
+  // Serial.println(ldrValue);
+  // delay(1000);
 
-  if (ldrValue<100) {
+  if (ldrValue>THRESHOLD) {
     tone(buzzerPin, 1000);
     delay(1000); 
     noTone(buzzerPin);
